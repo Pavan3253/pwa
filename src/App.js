ead49f4780577d0c1eba2c726298e2fdae03cd6d
Ninjago Organization <Pavan3253@outlook.com>
2021-10-30 19:55:29 +0530

    test commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { fecthWeather } from "./api/fetchWeather";
 import './App.css';
 
 const App = () => {
+    const [query, setQuery] = useState('');
+
+    const search = async (e) => {
+        if(e.key === 'Enter'){
+            const data = await fecthWeather(query)
+
+            console.log(data);
+        }
+    }
+
     return(
-        <h1>App</h1>
+        <div className="main-container">
+            <input 
+                type="text"
+                className="search"
+                placeholder="Search Countries Weather"
+                value={query}
+                onChange={(e) => setQuery(e.target.value)}
+                onKeyPress={search}
+            />
+        </div>
     );
 }
 
